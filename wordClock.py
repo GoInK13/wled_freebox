afc2a40f568e9577098d7fee9dcb0fe6ad60a019
Pierre <p.lescure@trilys.com>
2023-01-05 18:45:48 +0100

    Restore previous brightness

--- wordClock.py
+++ wordClock.py
@@ -35,7 +35,33 @@ fbx = Freepybox()
 # Be ready to authorize the application on the Freebox.
 fbx.open('192.168.1.254')
 
+def CheckFixIp(json):
+	isFix=False
+	print("\n-----\njson="+str(json))
+	print("IP:"+str(json["l3connectivities"][0]["addr"]))
+	realName=""
+	realIp=""
+	for name in json["names"]:
+		realName+=name["name"]+","
+	for address in json["l3connectivities"]:
+		if address["active"]==True and address["af"]=="ipv4":
+			realIp=str(address["addr"])
+			if int(address["addr"].replace("192.168.1.",""))>200:
+				isFix=True
+	print(str(realName)+"="+str(realIp)+"="+str(isFix))
+	return isFix
+
+def GetBrightness():
+	try:
+		x = requests.get("http://192.168.1.241/win")
+		value=str(x.content)
+		return int(value[value.index("<ac>")+4:value.index("</ac>")])
+	except Exception as e:
+		return 20
+
 counterUserOld=0
+oldValue=GetBrightness()
+
 while True:
 	try:
 		fbx_lan_host = fbx.lan.get_hosts_list()
@@ -44,14 +70,23 @@ while True:
 	counterUser=0
 	for jsHost in fbx_lan_host:
 		if jsHost["active"]==True:
-#			print("jsHost="+str(jsHost["names"]))
-#			print(str(jsHost))
-			counterUser+=1
+			if CheckFixIp(jsHost)==False:
+				counterUser+=1
+	print("Number user = "+str(counterUser))
 	if counterUserOld!=counterUser:
-		if counterUser>4:
-			x = requests.get('http://192.168.1.241/win&A=255')
+		if counterUser>0:
+			if oldValue<=20:
+				oldValue=255
+			try:
+				x = requests.get('http://192.168.1.241/win&A='+str(oldValue))
+			except Exception as e:
+				pass
 		else:
-			x = requests.get('http://192.168.1.241/win&A=20')
+			oldValue=GetBrightness()
+			try:
+				x = requests.get('http://192.168.1.241/win&A=20')
+			except Exception as e:
+				pass
 		counterUserOld=counterUser
 	time.sleep(30)
 
