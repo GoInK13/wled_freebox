2b69247cd25c16e5ab5d2c610239d8ef11b2f7f1
Pierrot <pierr.lescure+git@gmail.com>
2025-02-21 10:25:14 +0100

    Update all scripts and README

--- README.md
+++ README.md
@@ -4,26 +4,54 @@ Simple script to reduce brightness of WLED device on freebox server (using Freeb
 ## Details
 It uses [freepybox](https://github.com/fstercq/freepybox)
 
-## Installation
-    pip3 install freepybox
+## Installation on debian  
+
+```
+git clone https://github.com/GoInK13/wled_freebox.git
+cd wled_freebox
+sudo apt update
+sudo apt install python3-pip
+python3 -m venv venv-wordclock
+venv-wordclock/bin/pip3 install freebox-api
+venv-wordclock/bin/pip3 install requests
+```
+
+## Test
+
+```
+source venv-wordclock/bin/activate
+python3 wordClock.py
+```
 
 ## Add service
+
 Create a new service :
-    sudo nano /etc/systemd/system/wordClock.service
+
+```
+sudo nano /etc/systemd/system/wordClock.service
+```
 
 And paste the next text :
 
-    #[Unit]
-    #Description=Word clock script to set brightness
-    #After=default.target
-    
-    #[Service]
-    #ExecStart=/home/pierrot/wordClock.py
-    #User=pierrot
-    
-    #[Install]
-    #WantedBy=default.target
+```
+[Unit]
+Description=Word clock script to set brightness
+After=default.target
+
+[Service]
+WorkingDirectory=/home/pierrot/servers/wled_freebox
+ExecStart=/home/pierrot/servers/wled_freebox/venv-wordclock/bin/python3 /home/pierrot/servers/wled_freebox/wordClock.py
+#Restart=on-failure
+User=pierrot
+
+[Install]
+WantedBy=default.target
+```
 
 ### Start the service 
-    sudo systemctl daemon-reload
-    sudo systemctl enable wordClock.service
+
+```
+sudo systemctl daemon-reload
+sudo systemctl enable wordClock.service
+sudo systemctl start wordClock.service
+```
--- wordClock.py
+++ wordClock.py
@@ -24,99 +24,109 @@
 
 
 # Import the freepybox package.
-from freepybox import Freepybox
+import asyncio
+from freebox_api import Freepybox
 import requests
 import time
 
-PRINT_ENABLE = 1 #0: Disable print, 1:Enable
+PRINT_ENABLE = 0 #0: Disable print, 1:Enable
 POWER_ON = 255
 POWER_OFF = 5
 
-# Instantiate the Freepybox class using default options.
-fbx = Freepybox()
+async def main():
+	# Instantiate the Freepybox class using default options.
+	fbx = Freepybox(api_version="latest")
 
-# Connect to the freebox with default options.
-# Be ready to authorize the application on the Freebox.
-fbx.open('192.168.1.254')
-
-def CheckFixIp(json):
-	isFix=False
+	# Connect to the freebox with default options.
+	# Be ready to authorize the application on the Freebox.
+	await fbx.open(host='mafreebox.freebox.fr', port=443)
 	if PRINT_ENABLE==1:
-		print("\n-----\njson="+str(json))
-		print("IP:"+str(json["l3connectivities"][0]["addr"]))
-	realName=""
-	realIp=""
-	try:
-		for name in json["names"]:
-			realName+=name["name"]+","
-		for address in json["l3connectivities"]:
-			if address["active"]==True and address["af"]=="ipv4":
-				realIp=str(address["addr"])
-				if int(address["addr"].replace("192.168.1.",""))>200:
-					isFix=True
-	except Exception as e:
+		print("Connection success")
+
+	def CheckFixIp(json):
+		isFix=False
 		if PRINT_ENABLE==1:
-			print("Exception : "+str(e))
-	if PRINT_ENABLE==1:
-		print(str(realName)+"="+str(realIp)+"="+str(isFix))
-	return isFix
-
-def GetBrightness():
-	try:
-		x = requests.get("http://192.168.1.241/win")
-		value=str(x.content)
-		return int(value[value.index("<ac>")+4:value.index("</ac>")])
-	except Exception as e:
-		return 20
-
-def CountUser():
-	counterUser=0
-	for jsHost in fbx_lan_host:
-		if jsHost["active"]==True:
-			if CheckFixIp(jsHost)==False:
-				counterUser+=1
-	return counterUser
-
-counterUserOld=0
-oldValue=GetBrightness()
-
-reConnect=False
-
-while True:
-	try:
-		fbx_lan_host = fbx.lan.get_hosts_list()
-	except Exception as e:
-		reConnect=True
-	if reConnect==True:
-		fbx.open('192.168.1.254')
-		time.sleep(10)
-		reConnect=False
-		pass
-	try:
-		counterUser=CountUser()
-	except Exception as e:
+			print("\n-----\njson="+str(json))
+			print("IP:"+str(json["l3connectivities"][0]["addr"]))
+		realName=""
+		realIp=""
+		try:
+			for name in json["names"]:
+				realName+=name["name"]+","
+			for address in json["l3connectivities"]:
+				if address["active"]==True and address["af"]=="ipv4":
+					realIp=str(address["addr"])
+					if int(address["addr"].replace("192.168.1.",""))>200:
+						isFix=True
+		except Exception as e:
+			if PRINT_ENABLE==1:
+				print("Exception : "+str(e))
 		if PRINT_ENABLE==1:
-			print("Exception during counter user:"+str(e))
+			print(str(realName)+"="+str(realIp)+"="+str(isFix))
+		return isFix
+
+	def GetBrightness():
+		try:
+			x = requests.get("http://192.168.1.241/win")
+			value=str(x.content)
+			return int(value[value.index("<ac>")+4:value.index("</ac>")])
+		except Exception as e:
+			return 20
+
+	def CountUser():
 		counterUser=0
-	if PRINT_ENABLE==1:
-		print("Number user = "+str(counterUser))
-	if counterUserOld!=counterUser:
-		if counterUser>0:
-			if oldValue<=20:
-				oldValue=255
-			if str(GetBrightness())!='0':
+		for jsHost in fbx_lan_host:
+			if jsHost["active"]==True:
+				if CheckFixIp(jsHost)==False:
+					counterUser+=1
+		return counterUser
+
+	counterUserOld=0
+	oldValue=GetBrightness()
+
+	reConnect=False
+
+	while True:
+		try:
+			fbx_lan_host = await fbx.lan.get_hosts_list()
+		except Exception as e:
+			if PRINT_ENABLE==1:
+				print("Exception during get_hosts_list():"+str(e))
+			reConnect=True
+		if reConnect==True:
+			await fbx.open('mafreebox.freebox.fr', port=443)
+			time.sleep(10)
+			reConnect=False
+			pass
+		try:
+			counterUser=CountUser()
+		except Exception as e:
+			if PRINT_ENABLE==1:
+				print("Exception during counter user:"+str(e))
+			counterUser=0
+		if PRINT_ENABLE==1:
+			print("Number user = "+str(counterUser))
+		if counterUserOld!=counterUser:
+			if counterUser>0:
+				if oldValue<=20:
+					oldValue=255
+				if str(GetBrightness())!='0':
+					try:
+						x = requests.get('http://192.168.1.241/win&A='+str(oldValue))
+					except Exception as e:
+						pass
+			else:
+				oldValue=GetBrightness()
 				try:
-					x = requests.get('http://192.168.1.241/win&A='+str(oldValue))
+					x = requests.get('http://192.168.1.241/win&A='+str(POWER_OFF))
 				except Exception as e:
 					pass
-		else:
-			oldValue=GetBrightness()
-			try:
-				x = requests.get('http://192.168.1.241/win&A='+str(POWER_OFF))
-			except Exception as e:
-				pass
-		counterUserOld=counterUser
-	time.sleep(30)
+			counterUserOld=counterUser
+		time.sleep(30)
 
-# Properly close the session.
-fbx.close()
+	# Properly close the session.
+	await fbx.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
