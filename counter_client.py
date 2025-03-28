3d324936dd15be416d670297fb600acb01a38ad9
Pierrot <pierr.lescure+git@gmail.com>
2025-03-28 17:30:13 +0100

    Replace find IP with string instead of array, to avoid exception

--- counter_client.py
+++ counter_client.py
@@ -54,107 +54,134 @@ def publish(client):
             break
 
 async def main(client):
-	# Instantiate the Freepybox class using default options.
-	fbx = Freepybox(api_version="latest")
+    # Instantiate the Freepybox class using default options.
+    fbx = Freepybox(api_version="latest")
 
-	# Connect to the freebox with default options.
-	# Be ready to authorize the application on the Freebox.
-	await fbx.open(host='mafreebox.freebox.fr', port=443)
-	if PRINT_ENABLE==1:
-		print("Connection success")
+    # Connect to the freebox with default options.
+    # Be ready to authorize the application on the Freebox.
+    await fbx.open(host='mafreebox.freebox.fr', port=443)
+    if PRINT_ENABLE==1:
+        print("Connection success")
 
-	def CheckFixIp(json):
-		isFix=False
-		if PRINT_ENABLE==1:
-			print("\n-----\njson="+str(json))
-			print("IP:"+str(json["l3connectivities"][0]["addr"]))
-		realName=""
-		realIp=""
-		try:
-			for name in json["names"]:
-				realName+=name["name"]+","
-			for address in json["l3connectivities"]:
-				if address["active"]==True and address["af"]=="ipv4":
-					realIp=str(address["addr"])
-					if int(address["addr"].replace("192.168.1.",""))>200:
-						isFix=True
-		except Exception as e:
-			isFix=True
-			if PRINT_ENABLE==1:
-				print("Exception : "+str(e))
-		if PRINT_ENABLE==1:
-			print(str(realName)+"="+str(realIp)+"="+str(isFix))
-		return isFix
+    def CheckFixIp(json):
+        isFix=False
+        if PRINT_ENABLE==1:
+            print("\n-----\njson="+str(json))
+            print("IP:"+str(json["l3connectivities"][0]["addr"]))
+        realName=""
+        try:
+            __find_start = str(json).find("'primary_name'")+17
+            __find_stop  = str(json).find("'", __find_start)
+            realName=str(json)[__find_start:__find_stop]
+        except Exception as e:
+            realName="Unknown"
+            if PRINT_ENABLE==1:
+                print("Exception : "+str(e))
+        try:
+            __find_start = str(json).find("'192.168.1")+1
+            __find_stop  = str(json).find("'", __find_start)
+            realIp=str(json)[__find_start:__find_stop]
+        except Exception as e:
+            realIp="192.168.1.255"
+            if PRINT_ENABLE==1:
+                print("Exception : "+str(e))
+        if int(realIp.replace("192.168.1.",""))>=200:
+            isFix=True
+        return isFix
 
-	def GetBrightness():
-		try:
-			x = requests.get("http://192.168.1.241/win")
-			value=str(x.content)
-			return int(value[value.index("<ac>")+4:value.index("</ac>")])
-		except Exception as e:
-			return 20
+    def CheckFixIp_old(json):
+        isFix=False
+        if PRINT_ENABLE==1:
+            print("\n-----\njson="+str(json))
+            print("IP:"+str(json["l3connectivities"][0]["addr"]))
+        realName=""
+        realIp=""
+        try:
+            for name in json["names"]:
+                realName+=name["name"]+","
+            for address in json["l3connectivities"]:
+                if address["active"]==True and address["af"]=="ipv4":
+                    realIp=str(address["addr"])
+                    if int(address["addr"].replace("192.168.1.",""))>200:
+                        isFix=True
+        except Exception as e:
+            isFix=True
+            if PRINT_ENABLE==1:
+                print("Exception : "+str(e))
+        if PRINT_ENABLE==1:
+            print(str(realName)+"="+str(realIp)+"="+str(isFix))
+        return isFix
 
-	def CountUser():
-		counterUser=0
-		for jsHost in fbx_lan_host:
-			if jsHost["active"]==True:
-				if CheckFixIp(jsHost)==False:
-					counterUser+=1
-					print("+1:"+str(jsHost))
-		return counterUser
+    def GetBrightness():
+        try:
+            x = requests.get("http://192.168.1.241/win")
+            value=str(x.content)
+            return int(value[value.index("<ac>")+4:value.index("</ac>")])
+        except Exception as e:
+            return 20
 
-	counterUserOld=0
-	oldValue=GetBrightness()
+    def CountUser():
+        counterUser=0
+        for jsHost in fbx_lan_host:
+            if jsHost["active"]==True:
+                if CheckFixIp(jsHost)==False:
+                    counterUser+=1
+                    if PRINT_ENABLE==1:
+                        print("+1:"+str(jsHost))
+        return counterUser
 
-	reConnect=False
+    counterUserOld=0
+    oldValue=GetBrightness()
 
-	while True:
-		try:
-			fbx_lan_host = await fbx.lan.get_hosts_list()
-		except Exception as e:
-			if PRINT_ENABLE==1:
-				print("Exception during get_hosts_list():"+str(e))
-			reConnect=True
-		if reConnect==True:
-			await fbx.open('mafreebox.freebox.fr', port=443)
-			time.sleep(10)
-			reConnect=False
-			pass
-		try:
-			counterUser=CountUser()
-		except Exception as e:
-			if PRINT_ENABLE==1:
-				print("Exception during counter user:"+str(e))
-			counterUser=0
-		if PRINT_ENABLE==1:
-			print("Number user = "+str(counterUser))
-		if False and counterUserOld!=counterUser:
-			if counterUser>0:
-				if oldValue<=20:
-					oldValue=255
-				if str(GetBrightness())!='0':
-					try:
-						x = requests.get('http://192.168.1.241/win&A='+str(oldValue))
-					except Exception as e:
-						pass
-			else:
-				oldValue=GetBrightness()
-				try:
-					x = requests.get('http://192.168.1.241/win&A='+str(POWER_OFF))
-				except Exception as e:
-					pass
-			counterUserOld=counterUser
-		result = client.publish(topic, counterUser)
-		# result: [0, 1]
-		status = result[0]
-		if status == 0:
-			print("Send "+str(counterUser)+" to topic "+str(topic))
-		else:
-			print("Failed to send message "+str(counterUser)+" to topic "+str(topic))
-		time.sleep(30)
+    reConnect=False
 
-	# Properly close the session.
-	await fbx.close()
+    while True:
+        try:
+            fbx_lan_host = await fbx.lan.get_hosts_list()
+        except Exception as e:
+            if PRINT_ENABLE==1:
+                print("Exception during get_hosts_list():"+str(e))
+            reConnect=True
+        if reConnect==True:
+            await fbx.open('mafreebox.freebox.fr', port=443)
+            time.sleep(10)
+            reConnect=False
+            pass
+        try:
+            counterUser=CountUser()
+        except Exception as e:
+            if PRINT_ENABLE==1:
+                print("Exception during counter user:"+str(e))
+            counterUser=0
+        if PRINT_ENABLE==1:
+            print("Number user = "+str(counterUser))
+        if False and counterUserOld!=counterUser:
+            if counterUser>0:
+                if oldValue<=20:
+                    oldValue=255
+                if str(GetBrightness())!='0':
+                    try:
+                        x = requests.get('http://192.168.1.241/win&A='+str(oldValue))
+                    except Exception as e:
+                        pass
+            else:
+                oldValue=GetBrightness()
+                try:
+                    x = requests.get('http://192.168.1.241/win&A='+str(POWER_OFF))
+                except Exception as e:
+                    pass
+            counterUserOld=counterUser
+        result = client.publish(topic, counterUser)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print("Send "+str(counterUser)+" to topic "+str(topic))
+        else:
+            print("Failed to send message "+str(counterUser)+" to topic "+str(topic))
+        time.sleep(30)
+
+    # Properly close the session.
+    await fbx.close()
 
 
 def run():
